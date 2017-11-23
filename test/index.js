0884c4decd10ab333f5a88640baf8b2aea0d1023
wemlion <angusfu1126@qq.com>
2017-11-24 00:32:11 +0800

    fix: use sync function

--- test/index.js
+++ test/index.js
@@ -5,6 +5,6 @@ const svgstr = fs.readFileSync('./test/test.svg').toString()
 console.log('Before parsing: ', svgstr)
 
 parse.parse('./test/test.svg')
-fs.writeFile('./test/result.svg', parse.toSimpleSvg(), 'utf-8')
+fs.writeFileSync('./test/result.svg', parse.toSimpleSvg(), 'utf-8')
 
 console.log('Parsing result: ', parse.toSimpleSvg())
