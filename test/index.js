fc6e22b4b51e10f2b36ea9f6a2610ed4d1e8a56d
gaolu11 <768065158@qq.com>
2019-03-01 18:40:29 +0800

    fix eslint

--- test/index.js
+++ test/index.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
-const parse = require('../lib/index.js');
-const svgstr = fs.readFileSync('./test/test.svg').toString();
+const fs = require('fs')
+const parse = require('../lib/index.js')
+// eslint-disable-next-line no-unused-vars
+const svgstr = fs.readFileSync('./test/test.svg').toString()
 
-
-parse.parse("./test/test.svg")
+parse.parse('./test/test.svg')
 console.log(parse.toSimpleSvg())
 
-fs.writeFile("./test/result.svg", parse.toSimpleSvg(), "utf-8");
+fs.writeFile('./test/result.svg', parse.toSimpleSvg(), 'utf-8')
